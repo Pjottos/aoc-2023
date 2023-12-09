3ec6dc1511ebae0ba599a26c6e94005945902d01
Pjottos <pjottos@proton.me>
2023-12-09 11:21:51 +0000

    Optimize day 9

--- src/d09.rs
+++ src/d09.rs
@@ -4,44 +4,63 @@ pub fn run(h: &mut Harness) {
     h.begin(9)
         .run_part(1, |text| {
             let mut sum = 0;
-            for line in text.lines() {
-                let mut nums = line
-                    .split(' ')
-                    .map(|n| n.parse::<i32>().unwrap())
-                    .collect::<Vec<_>>();
-                let mut lasts = vec![];
-                while !nums.iter().all(|&n| n == 0) {
-                    for n in 0..nums.len() - 1 {
-                        nums[n] = nums[n + 1] - nums[n];
+            let mut nums = vec![];
+            let mut num = 0;
+            let mut sign = false;
+            for &b in text.as_bytes() {
+                if b == b'-' {
+                    sign = true;
+                } else if b == b' ' {
+                    nums.push(if sign { -num } else { num });
+                    num = 0;
+                    sign = false;
+                } else if b != b'\n' {
+                    num = (num * 10) + i32::from(b - b'0');
+                } else {
+                    nums.push(if sign { -num } else { num });
+                    num = 0;
+                    sign = false;
+
+                    while nums.iter().any(|&n| n != 0) {
+                        for n in 0..nums.len() - 1 {
+                            nums[n] = nums[n + 1] - nums[n];
+                        }
+                        sum += nums.pop().unwrap();
                     }
-                    lasts.push(nums.pop().unwrap());
-                }
-                for num in lasts.iter().rev() {
-                    sum += num;
+                    nums.clear();
                 }
             }
             sum
         })
         .run_part(2, |text| {
             let mut sum = 0;
-            for line in text.lines() {
-                let mut nums = line
-                    .split(' ')
-                    .map(|n| n.parse::<i32>().unwrap())
-                    .collect::<Vec<_>>();
-                nums.reverse();
-                let mut firsts = vec![];
-                while !nums.iter().all(|&n| n == 0) {
-                    for n in 0..nums.len() - 1 {
-                        nums[n] = nums[n] - nums[n + 1];
+            let mut nums = vec![];
+            let mut num = 0;
+            let mut sign = false;
+            for &b in text.as_bytes() {
+                if b == b'-' {
+                    sign = true;
+                } else if b == b' ' {
+                    nums.push(if sign { -num } else { num });
+                    num = 0;
+                    sign = false;
+                } else if b != b'\n' {
+                    num = (num * 10) + i32::from(b - b'0');
+                } else {
+                    nums.push(if sign { -num } else { num });
+                    num = 0;
+                    sign = false;
+
+                    nums.reverse();
+
+                    while nums.iter().any(|&n| n != 0) {
+                        for n in 0..nums.len() - 1 {
+                            nums[n] = nums[n + 1] - nums[n];
+                        }
+                        sum += nums.pop().unwrap();
                     }
-                    firsts.push(nums.pop().unwrap());
+                    nums.clear();
                 }
-                let mut next = 0;
-                for num in firsts.iter().rev() {
-                    next = num - next;
-                }
-                sum += next;
             }
             sum
         });
